8eb63e6a70a8e8c6d32273afdffa21030c068fcb
wangchi <f2114198@163.com>
2018-05-29 16:40:20 +0800

    fix add feign can't start service

--- src/main/java/com/imooc/order/controller/ClientController.java
+++ src/main/java/com/imooc/order/controller/ClientController.java
@@ -1,5 +1,6 @@
 package com.imooc.order.controller;
 
+import com.imooc.order.client.ProductClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
@@ -12,26 +13,13 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class ClientController {
 
-    /* @Autowired // 第二种方法
-     private LoadBalancerClient loadBalancerClient;*/
     @Autowired
-    private RestTemplate restTemplate;
+    private ProductClient productClient;
 
     @GetMapping("/getProductMsg")
     public String getProductMsg() {
-        // 1. 第一种方式（直接使用restTemplate，url写死）
-       /* RestTemplate restTemplate = new RestTemplate();
-        String response = restTemplate.getForObject("http://localhost:8080/msg",String.class);
-        log.info("response={}",response);*/
-        // 2. 第二中方式(通过应用名字获取host信息，再次使用restTemplate）
-        /*ServiceInstance serviceInstance = loadBalancerClient.choose("PRODUCT");
-        String url = String.format("http://%s:%s", serviceInstance.getHost(),serviceInstance.getPort()+"/msg");
-        RestTemplate restTemplate = new RestTemplate();
-        String response = restTemplate.getForObject(url,String.class);
-        log.info("response={}", response);*/
 
-        // 3. 第三种方式（利用@LoadBalanced，可在restTemplate里使用应用名字）
-        String response = restTemplate.getForObject("http://PRODUCT/msg",String.class);
+        String response = productClient.productMsg();
         log.info("response={}", response);
         return response;
     }
