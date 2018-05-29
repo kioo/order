52c32f2f618ced55d3ddf9a589b12f505caac0a2
wangchi <f2114198@163.com>
2018-05-29 14:12:14 +0800

    complete order Controller

---
+++ src/test/java/com/imooc/order/repository/OrderMasterRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.imooc.order.repository;
+
+import com.imooc.order.OrderApplicationTests;
+import com.imooc.order.dataobject.OrderMaster;
+import com.imooc.order.enums.OrderStatusEnum;
+import com.imooc.order.enums.PayStatusEnum;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class OrderMasterRepositoryTest extends OrderApplicationTests {
+    @Autowired
+    private OrderMasterRepository orderMasterRepository;
+
+    @Test
+    public void testSave(){
+        OrderMaster orderMaster = new OrderMaster();
+        orderMaster.setOrderId("1234567");
+        orderMaster.setBuyerName("师妹");
+        orderMaster.setBuyerPhone("135468468916");
+        orderMaster.setBuyerAddress("长宁六路");
+        orderMaster.setBuyerOpenid("11011110");
+        orderMaster.setOrderAmount(new BigDecimal(2.5));
+        orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
+        orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
+
+        OrderMaster result =  orderMasterRepository.save(orderMaster);
+        Assert.assertTrue(result!=null);
+    }
+}
