cbb5bc671f82f42e8420197c52ced5051d7d1756
wangchi <f2114198@163.com>
2018-05-29 17:27:25 +0800

    fix client request

--- src/main/java/com/imooc/order/controller/ClientController.java
+++ src/main/java/com/imooc/order/controller/ClientController.java
@@ -1,6 +1,7 @@
 package com.imooc.order.controller;
 
 import com.imooc.order.client.ProductClient;
+import com.imooc.order.dataobject.ProductInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
@@ -8,6 +9,9 @@ import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -22,5 +26,12 @@ public class ClientController {
         String response = productClient.productMsg();
         log.info("response={}", response);
         return response;
+    }
+
+    @GetMapping("/getProductList")
+    public String getProductList(){
+        List<ProductInfo> productInfoList = productClient.listForOrder(Arrays.asList("164103465734242707"));
+        log.info("response={}", productInfoList);
+        return "ok";
     }
 }
